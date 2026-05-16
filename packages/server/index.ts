3c6f9f325b574e30d4485bca8b017f8bab236975
Biplob Talukdar <biplob.codes@gmail.com>
2026-05-17 01:33:36 +0600

    Connect client to server

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,6 +4,10 @@ const app = express();
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!!!");
 });
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello from the Server!" });
+});
+
 const port = process.env.PORT || "3000";
 app.listen(port, () => {
   console.log(`Server is running on  http://localhost:${port}`);
