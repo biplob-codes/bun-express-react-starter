2aa57d26b128df3d7fdfdcaca85215e7d8603dba
Biplob Talukdar <biplob.codes@gmail.com>
2026-05-17 01:07:09 +0600

    Set up backend with express

---
+++ packages/server/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import type { Request, Response } from "express";
+const app = express();
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!!!");
+});
+const port = process.env.PORT || "3000";
+app.listen(port, () => {
+  console.log(`Server is running on  http://localhost:${port}`);
+});
